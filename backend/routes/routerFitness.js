a7b7302ccb32e7bc827fc678323e393f1a07c507
melgress <63718040+melgress@users.noreply.github.com>
2020-09-08 20:29:41 +0200

    added fitness_en routes

--- backend/routes/routerFitness.js
+++ backend/routes/routerFitness.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("../db.js");
 const routerFitness = express.Router();
 
-//Creating GET Router to fetch all the learner details from the MySQL Database
+//Get all courses
 routerFitness.get("/fitness", (req, res) => {
   db.query("SELECT * FROM fitness", (err, rows, fields) => {
     if (!err) res.send(rows);
@@ -10,7 +10,15 @@ routerFitness.get("/fitness", (req, res) => {
   });
 });
 
-//Get all courses
+//For English: Get all courses
+routerFitness.get("/fitness/en", (req, res) => {
+  db.query("SELECT * FROM fitness_en", (err, rows, fields) => {
+    if (!err) res.send(rows);
+    else console.log(err);
+  });
+});
+
+//Get course by id
 routerFitness.get("/fitness/:id", (req, res) => {
   db.query(
     "SELECT * FROM fitness WHERE id = ?",
@@ -21,6 +29,19 @@ routerFitness.get("/fitness/:id", (req, res) => {
     }
   );
 });
+
+//For English: Get course by id
+routerFitness.get("/fitness/en/:id", (req, res) => {
+  db.query(
+    "SELECT * FROM fitness_en WHERE id = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+});
+
 //add a course
 routerFitness.post("/addCourse", (req, res) => {
   let data = {
@@ -33,6 +54,21 @@ routerFitness.post("/addCourse", (req, res) => {
   db.query(sql, data, (err, results) => {
     if (err) throw err;
     res.redirect("/api/fitness");
+  });
+});
+
+//For English: add a course
+routerFitness.post("/addCourse/en", (req, res) => {
+  let data = {
+    date: req.body.date,
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  };
+  let sql = "INSERT INTO fitness_en SET ?";
+  db.query(sql, data, (err, results) => {
+    if (err) throw err;
+    res.redirect("/api/fitness/en");
   });
 });
 
@@ -58,12 +94,44 @@ routerFitness.put("/fitness/:id", function (req, res) {
   );
 });
 
+//For English: update a course
+routerFitness.put("/fitness/en/:id", function (req, res) {
+  let data = {
+    date: req.body.date,
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  };
+  console.log("Put received");
+  db.query(
+    "UPDATE fitness_en SET ? WHERE id = ?",
+    [data, req.params.id],
+    (err, result) => {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      }
+      res.send(data);
+    }
+  );
+});
+
 //delete a course
 routerFitness.delete("/fitness/:id", (req, res) => {
   db.query("DELETE FROM fitness WHERE id = ?", [req.params.id], (err) => {
     if (!err) {
       console.log("Fitness deleted successfully.");
       res.redirect("/api/fitness");
+    } else console.log(err);
+  });
+});
+
+//For English: delete a course
+routerFitness.delete("/fitness/en/:id", (req, res) => {
+  db.query("DELETE FROM fitness_en WHERE id = ?", [req.params.id], (err) => {
+    if (!err) {
+      console.log("Fitness deleted successfully.");
+      res.redirect("/api/fitness/en");
     } else console.log(err);
   });
 });
